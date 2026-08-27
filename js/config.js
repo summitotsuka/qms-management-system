c0d24caf1c31fdc51babffd69f693056bc3235da
CHONDAN <taximeter96@hotmail.com>
2026-08-27 16:57:01 +0700

    Add application name and version to config

--- js/config.js
+++ js/config.js
@@ -1,3 +1,7 @@
 const CONFIG = {
+  APP_NAME: 'Integrated Management System',
+  APP_SHORT_NAME: 'IMS',
+  VERSION: '1.0.0',
+
   API_URL: 'https://script.google.com/macros/s/AKfycbzJLxtSznc1ozR-UMLme7apjJyiCIkJnEpdsytZpZWD79NIAqWKJnFPXtdPXzZcZi-DIQ/exec'
 };
